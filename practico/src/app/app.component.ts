eb407f5508cafd59b112ebafc1365a944831aa66
faria <faria@DESKTOP-TLUUL68>
2023-07-18 13:36:39 -0300

    terminada la tarea

--- practico/src/app/app.component.ts
+++ practico/src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+export type producto = {
+  name: string ,
+  price: number,
+
+};
+/* export type producto = {
+  name: string,
+ price: string,
+} */
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+ 
+ 
 })
+
 export class AppComponent {
   title = 'practico';
+  nombreBtn = 'Ocultar Tabla'
+
+
+
+  productos : producto[] = [
+    { name: 'Jugo saldan 1L ', price: 200},
+    { name: 'Fideos Marolio', price: 100},
+    { name: 'Pure de tomate', price: 150},
+    { name: 'Coca Cola 2L', price: 500},
+    { name: 'Café 500gr', price: 1000},
+    { name: 'Carbon',  price: 700},
+    { name: 'Agua Mineral 1L',  price: 400},
+    { name: 'Jabon ',  price: 332},
+    { name: 'Papas Fritas',  price: 600},
+    { name: 'Flan', price: 295},
+  ];
+
+ constructor(private modalService: NgbModal) {
+
+ }
+
+ public open(modal: any): void {
+
+   this.modalService.open(modal);
+
+ }
+
+ mostrarTabla(){
+  return this.nombreBtn !== 'Mostar Tabla';
+ }
+
+ cambiarNombreBtn(nombre:string){
+  nombre === 'Ocultar Tabla' ? this.nombreBtn = "Mostar Tabla" : this.nombreBtn = "Ocultar Tabla"
+
+ }
 }
--- practico/src/app/app.module.ts
+++ practico/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        NgbModule,
+    ]
 })
 export class AppModule { }
